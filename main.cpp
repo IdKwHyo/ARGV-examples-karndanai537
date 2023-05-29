6c8e31ae31f40eadc7d0deb49b999f9293306b5e
IdKwHyo <87351345+IdKwHyo@users.noreply.github.com>
2023-05-29 13:49:53 +0000

    redo newest

--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
+#define SIZE 10
 #include <iostream>
 #include <string.h>
 using namespace std;
+int main(int argc, char   **argv){
+ 
+ int i;
+ cout<<"My number of arguments input is "<<argc<<endl;
+ int N=argc-1;
+ int * a=new int[N];
+	for(i=0;i<N;i++) {
+			    a[i]=atoi(argv[i+1]);
+		         cout<< "a[" << i <<"]="<< a[i]<<endl;
+		}
+   delete[] a;
 
-int main(int argc, char **argv) {
-  int n, *a;
-  n = argc - 1;
-  a = new int[n];
-  
-  for (int i = 0; i < n; i++) {
-      a[i] = atoi(argv[i + 1]);
-      cout << "a[" << i << "]=" << a[i] << endl;
-  }
-
-  int *p = a;
-  int *pb = a + n - 1;
-  int temp;
-  for (int i = 0; i < n / 2; i++) {
-      temp = *p;
-      *p = *pb;
-      *pb = temp;
-      p++;
-      pb--;
-  }
-
-    p = a;
-
-    cout << "Reversed " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "a[" << i << "]=" << a[i] << endl;
-    }
-
-    delete[] a;
-    return 0;
+  return 0;
 }
